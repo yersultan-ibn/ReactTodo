72022003e2832d8109d61c597e70151378121b2e
Ersultan <ersultansansyzbaj012@gmail.com>
2022-09-14 19:39:26 +0600

    dom

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
